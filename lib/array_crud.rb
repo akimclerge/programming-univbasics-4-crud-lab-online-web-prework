98cc9afb710673612abc90fcf4b41df0020c7cf1
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-09-27 00:54:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,26 +1,33 @@
 def create_an_empty_array
+  []
   
 end
 
 def create_an_array
-  
+  family = [ "baby", "daddy", "mommy", "grandma"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["wow", "I", "am", "really", "learning"]
+  array.push("arrays!")
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+   array = ["I", "am", "really", "learning"]
+    array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
+ array = ["I", "am", "really", "learning", "arrays!"]
+ array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
+   a3rray = ["I", "am", "really", "learning", "arrays!"]
   
-end
+end3333
 
 def retrieve_element_from_index(array, index_number)
   
